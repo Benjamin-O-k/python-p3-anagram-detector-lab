a8053b5144e24843e957c7de42d9065ac31ec8e4
Benjamin Okello <benjamin.okello@student.moringaschool.com>
2024-09-03 15:36:40 +0300

    passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+    def match(self, list):
+        matches= []
+        for word in list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                matches.append(word)
+        return matches
